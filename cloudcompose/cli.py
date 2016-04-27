dfa40cc937b843618e910e6c48d5c769b4da42c5
Patrick Cullen <patrick.cullen@washpost.com>
2016-04-26 21:29:36 -0400

    removed extra print statement

--- cloudcompose/cli.py
+++ cloudcompose/cli.py
@@ -5,7 +5,7 @@ import click
 import os
 from importlib import import_module
 
-#TODO read the list of plugins from the docker-compose.yml
+#TODO read the list of plugins from the cloud-compose.yml
 plugins = ['cluster']
 
 class CLI(click.MultiCommand):
@@ -14,12 +14,11 @@ class CLI(click.MultiCommand):
 
     def get_command(self, ctx, name):
         module_name = "cloudcompose.%s.commands.cli" % name
-        print "Importing %s" % module_name
         imported_module = import_module(module_name)
         return imported_module.cli
 
-cli = CLI(help='This tool\'s subcommands are loaded from a '
-            'plugin folder dynamically.')
+cli = CLI(help='This tool\'s subcommands are loaded from the cloudcompose.<plugin>.commands.cli package '
+          'where <plugin> is defined in the cloud-compose.yml file')
 
 if __name__ == '__main__':
     cli()
