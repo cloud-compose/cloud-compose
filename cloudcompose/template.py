65788233ed71c097958af71dc699a5597ddfd159
Patrick Cullen <patrick.cullen@washpost.com>
2016-04-28 10:48:16 -0400

    fixed print bug

--- cloudcompose/template.py
+++ cloudcompose/template.py
@@ -1,12 +1,8 @@
 import jinja2
-from pprint import pprint
 from os.path import join
 
 class Template:
-    def render(self, plugin_name, cloud_config):
-        config_dir, config_data = cloud_config.config_data(plugin_name)
-        pprint(config_data)
-        template_file = join(config_dir, config_data['template'])
+    def render(self, template_file, template_data):
         template_obj = jinja2.Template(open(template_file).read())
-        return template_obj.render(config_data)
+        return template_obj.render(template_data)
 
