736d13670d20e3c17019675c7efcbfaa89864061
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-03 11:33:38 -0400

    changed loading method to allow for template includes

--- cloudcompose/template.py
+++ cloudcompose/template.py
@@ -2,7 +2,10 @@ import jinja2
 from os.path import join
 
 class Template:
+    def __init__(self, template_dir):
+        self.env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+
     def render(self, template_file, template_data):
-        template_obj = jinja2.Template(open(template_file).read())
+        template_obj = self.env.get_template(template_file)
         return template_obj.render(template_data)
 
